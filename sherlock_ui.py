f8570012f0389856bb8743f3ea29784d715dbc49
wolfey <wolfe.aarond@gmail.com>
2020-04-22 15:02:38 -0400

    Switching to venv

---
+++ sherlock_ui.py
@@ -0,0 +1,10 @@
+Error: No such file or directory: "sherlock.ui"
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'sherlock.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.3
+#
+# WARNING! All changes made in this file will be lost!
+
+
