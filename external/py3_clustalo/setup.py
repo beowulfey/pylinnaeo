42d60ad520c7f58dbf13ab60c9e09f137a4131fd
wolfey <wolfe.aarond@gmail.com>
2020-04-30 09:27:45 -0400

    structural changes

--- external/py3_clustalo/setup.py
+++ external/py3_clustalo/setup.py
@@ -12,7 +12,6 @@ if not OPENMP_DISABLED:
 
 module = Extension('clustalo',
                    sources = ['clustalo.c'],
-<<<<<<< HEAD
                    include_dirs=['./clustalo/include/clustalo'],
                    libraries=libraries,
                    library_dirs=['./clustalo/lib'],
@@ -22,18 +21,5 @@ setup(name='clustalo',
       version='0.2.0',
       description='Python3 wrapper around libclustalo',
       author='Aaron Wolfe',
-      url='https://github.com/beowolfey/clustalo-python3',
-=======
-                   include_dirs=['/usr/local/sci/clustalo/current/include/clustalo', '/usr/include/clustalo', '/usr/local/include/clustalo'],
-                   libraries=libraries,
-                   library_dirs=['/usr/local/sci/clustalo/current/lib'],
-                   extra_compile_args=extra_compile_args)
-
-setup(name='clustalo',
-      version='0.1.2',
-      description='Python wrapper around libclustalo',
-      author='Joshua Ma',
-      author_email='josh@benchling.com',
-      url='https://github.com/benchling/clustalo-python',
->>>>>>> 6fac9edf2b8218a8f2e964f9bddba90ff0ba479e
+      url='https://github.com/beowulfey/clustalo-python3',
       ext_modules=[module])
