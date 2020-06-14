786df9ec160dd0a7a3d6fcdcc4b5f624abd94936
Aaron Wolfe <wolfe.aarond@gmail.com>
2020-06-14 18:45:24 -0400

    turned off debug

--- linnaeo/main.py
+++ linnaeo/main.py
@@ -167,7 +167,7 @@ class Linnaeo(QMainWindow, methods.Slots, methods.Debug, linnaeo_ui.Ui_MainWindo
         self.mainLogger.debug("After StatusbarUpdate")
 
         # Load
-        self.DEBUG()  # TODO: DELETE THIS NEPHEW
+        #self.DEBUG()  # TODO: DELETE THIS NEPHEW
 
         #self.pdbWindow = displays.NGLviewer(self)
 
